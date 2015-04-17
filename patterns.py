e9135d7378909f5b62b1ee71004151d6380aabd8
abcdw <andrewtropin@gmail.com>
2015-04-17 13:17:20 +0300

    Add multiline JavaScript TypeError pattern

--- patterns.py
+++ patterns.py
@@ -45,4 +45,15 @@ pre_patterns = [
 main_patterns = [
     r'Expected an even-sized list of pairs \(k1, v1, \.\.\. kN, vN\), got \(p0\)',
     r'Names not in dictionary:',
+    r"""JavaScript. TypeError: Object #<t> has no method 'Ukraine'
+    at Object.blocks.b-counters__gemius (web3_exp/pages-desktop/search/_search.all.priv.js:POSITION_SUBSTITUTE)
+    at Object.[object Function].Object.toString.call.e.(anonymous function) [as b-counters__gemius] (web3_exp/pages-desktop/search/_search.all.priv.js:POSITION_SUBSTITUTE)
+    at Object.blocks.b-counters.blocks.b-cntrs (web3_exp/pages-desktop/search/_search.all.priv.js:POSITION_SUBSTITUTE)
+    at Object.[object Function].Object.toString.call.e.(anonymous function) [as b-counters] (web3_exp/pages-desktop/search/_search.all.priv.js:POSITION_SUBSTITUTE)
+    at Object.blocks.b-page__content (web3_exp/pages-desktop/search/_search.all.priv.js:POSITION_SUBSTITUTE)
+    at Object.blocks.b-page__content (web3_exp/pages-desktop/search/_search.all.priv.js:POSITION_SUBSTITUTE)
+    at Object.[object Function].Object.toString.call.e.(anonymous function) [as b-page__content] (web3_exp/pages-desktop/search/_search.all.priv.js:POSITION_SUBSTITUTE)
+    at blocks.b-page (web3_exp/pages-desktop/search/_search.all.priv.js:POSITION_SUBSTITUTE)
+    at Object.blocks.b-page (web3_exp/pages-desktop/search/_search.all.priv.js:POSITION_SUBSTITUTE)
+    at Object.[object Function].Object.toString.call.e.(anonymous function) [as b-page] (web3_exp/pages-desktop/search/_search.all.priv.js:POSITION_SUBSTITUTE) at /db/BASE/upper-000-1426519150/arkanavt/report/lib/YxWeb/Util/Template/JS.pm LINE_SUBSTITUTE. (Object.blocks.b-page (web3_exp/pages-desktop/search/_search.all.priv.js:POSITION_SUBSTITUTE))""",
 ]
